0d6266625172dc5804edd12cd5087e00d49565e4
Neil Forrester <neil@nforrester.net>
2020-02-02 01:00:25 -0800

    Add a variable length bit field supporting more or less efficient asking of which bits are high.

---
+++ src/blueprint/Bits.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <algorithm>
+
+#include <iostream> // TODO remove
+
+class Bits
+{
+private:
+    using Block = uint64_t;
+    static constexpr int_fast8_t bits_per_block = sizeof(Block) * 8;
+    static_assert(bits_per_block == 64);
+
+public:
+    Bits(size_t size): _size(size), _storage(size/bits_per_block + 1, 0)
+    {
+    }
+
+    void clear()
+    {
+        for (Block & block : _storage)
+        {
+            block = 0;
+        }
+    }
+
+    void set(size_t bit)
+    {
+        size_t block_num = bit / bits_per_block;
+        size_t subbit_num = bit % bits_per_block;
+        _storage[block_num] |= static_cast<Block>(1) << subbit_num;
+    }
+
+    bool get(size_t bit)
+    {
+        size_t block_num = bit / bits_per_block;
+        size_t subbit_num = bit % bits_per_block;
+        return _storage[block_num] & (static_cast<Block>(1) << subbit_num);
+    }
+
+    Bits const & operator&=(Bits const & other)
+    {
+        for (size_t block_num = 0; block_num < _storage.size(); ++block_num)
+        {
+            _storage[block_num] &= other._storage[block_num];
+        }
+        return *this;
+    }
+
+    bool any()
+    {
+        for (size_t block_num = 0; block_num < _storage.size(); ++block_num)
+        {
+            if (_storage[block_num])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void which(std::vector<size_t> & output)
+    {
+        size_t bits_in_blocks_not_already_done = _size;
+        for (size_t block_num = 0; block_num < _storage.size(); ++block_num)
+        {
+            Block block = _storage[block_num];
+            if (block == 0)
+            {
+                continue;
+            }
+
+            int_fast8_t const bits_in_this_block =
+                std::min<size_t>(bits_in_blocks_not_already_done, 64u);
+
+            int_fast8_t bit_num = 0;
+            int_fast8_t one_plus_upper_bound_for_next_high_bit = bits_in_this_block;
+            while (bit_num < bits_in_this_block)
+            {
+                int_fast8_t bits_left = one_plus_upper_bound_for_next_high_bit - bit_num;
+
+
+                Block half_mask = ((static_cast<Block>(1) << (bits_left / 2)) - 1) << bit_num;
+                Block full_mask = std::numeric_limits<Block>::max() << bit_num;
+
+                if ((block & full_mask) == 0)
+                {
+                    break;
+                }
+
+                if ((block & half_mask) == 0)
+                {
+                    bit_num += bits_left / 2;
+                }
+                else
+                {
+                    one_plus_upper_bound_for_next_high_bit = bits_left / 2 + bit_num;
+                }
+                if (one_plus_upper_bound_for_next_high_bit - 1 == bit_num)
+                {
+                    output.push_back(bit_num + bits_per_block * block_num);
+                    ++bit_num;
+                    one_plus_upper_bound_for_next_high_bit = bits_in_this_block;
+                }
+            }
+            bits_in_blocks_not_already_done -= bits_per_block;
+        }
+    }
+
+private:
+    size_t _size;
+    std::vector<Block> _storage;
+};
---
+++ src/blueprint/Bits_gtest.cpp
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+
+#include "Bits.h"
+
+TEST(BitsTest, Basic)
+{
+    Bits bits(10);
+    EXPECT_FALSE(bits.any());
+    bits.set(3);
+    EXPECT_TRUE(bits.any());
+    EXPECT_FALSE(bits.get(0));
+    EXPECT_FALSE(bits.get(1));
+    EXPECT_FALSE(bits.get(2));
+    EXPECT_TRUE(bits.get(3));
+    EXPECT_FALSE(bits.get(4));
+    EXPECT_FALSE(bits.get(5));
+    EXPECT_FALSE(bits.get(6));
+    EXPECT_FALSE(bits.get(7));
+    EXPECT_FALSE(bits.get(8));
+    EXPECT_FALSE(bits.get(9));
+    bits.clear();
+    EXPECT_FALSE(bits.any());
+
+    bits.set(0);
+    bits.set(5);
+    bits.set(9);
+
+    Bits bits2(10);
+    bits2.set(2);
+    bits2.set(5);
+
+    bits &= bits2;
+
+    EXPECT_FALSE(bits.get(0));
+    EXPECT_FALSE(bits.get(1));
+    EXPECT_FALSE(bits.get(2));
+    EXPECT_FALSE(bits.get(3));
+    EXPECT_FALSE(bits.get(4));
+    EXPECT_TRUE(bits.get(5));
+    EXPECT_FALSE(bits.get(6));
+    EXPECT_FALSE(bits.get(7));
+    EXPECT_FALSE(bits.get(8));
+    EXPECT_FALSE(bits.get(9));
+}
+
+TEST(BitsTest, Which)
+{
+    for (size_t size = 1; size < 5000; ++size)
+    {
+        Bits bits(size);
+        size_t n_sets = rand() % size;
+        for (size_t i = 0; i < n_sets; ++i)
+        {
+            bits.set(rand() % size);
+        }
+        for (int i = size - 1; i >= 0; --i)
+        {
+        }
+        std::vector<size_t> which;
+        bits.which(which);
+        size_t which_idx = 0;
+        for (size_t i = 0; i < size; ++i)
+        {
+            if (bits.get(i))
+            {
+                ASSERT_TRUE(which_idx < which.size());
+                ASSERT_EQ(i, which[which_idx]);
+                ++which_idx;
+            }
+            else
+            {
+                if (which_idx < which.size())
+                {
+                    ASSERT_NE(i, which[which_idx]);
+                }
+            }
+        }
+        ASSERT_EQ(which_idx, which.size());
+    }
+}
+
+int main(int argc, char ** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
